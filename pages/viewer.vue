1b39f859f2d93feffd0e64b42ac2d43a56998208
nathan <nerth001@fiu.edu>
2020-10-27 15:39:21 -0400

    removed fileBrowser.vue, probably easier code it inside viewer

--- pages/viewer.vue
+++ pages/viewer.vue
@@ -7,9 +7,40 @@
 
           <!-- Tree Menu for File Browsing -->
           <div class="2D-modeling col-2 bg-light border-right" style="height: 92vh">
-            Tree Menu for File Browsing <span class="badge bg-info ml-2 my-4">New</span>
-            <FileBrowser/>
+            <div class="fileBrowser">
+
+              <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                      class="navbar-toggler justify-content-center mt-3"
+                      data-target=".dual-collapse2"
+                      data-toggle="collapse" style="border:none; outline: none" type="button">
+                <a class="navbar-brand text-center font-weight-bold mx-auto pl-1" style="letter-spacing: 2px"></a>
+                {{ project_name }}</button>
+
+              <!-- Folder links -->
+              <div v-for="(folder, idx) of folder_list" :key="idx" class="collapse navbar-collapse w-100 dual-collapse2 my-3 ">
+                <button aria-controls="innerSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                        class="navbar-toggler justify-content-center mt-3"
+                        data-target=".dual-collapse2"
+                        data-toggle="collapse" style="border:none; outline: none" type="button">
+                  <a class="navbar-brand text-center font-weight-bold mx-auto pl-1" style="letter-spacing: 2px"></a>
+                  {{ folder }}</button>
+
+                <!-- Object Links -->
+                <div v-for="(object, idx) of object_list" :key="idx" class="collapse navbar-collapse w-100 dual-collapse2 my-3 ">
+                  <ul aria-controls="inner2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                      data-target=".dual-collapse2"
+                      data-toggle="collapse"
+                      class="navbar-nav ml-auto">
+                    <li class="nav-item text-center ">
+                      <a class="nav-link" style="cursor: pointer">{{ object }}</a>
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+            </div>
           </div>
+
 
           <!-- 3D Modeling Section -->
           <div class="3D-modeling col bg-transparent border-0 p-0 m-0" style="height: 92vh">
@@ -60,12 +91,11 @@ import navbar from "@/components/navbar";
 import myAwesomeExtension from "@/components/forge/extensions/myAwesomeExtension";
 import myCustomToolbar from "@/components/forge/extensions/myCustomToolbar";
 import '@/assets/stylesheets/viewer.css';
-import FileBrowser from "@/components/FileBrowser";
+import projects from "@/pages/projects";
 
 export default {
   name: "viewer",
   components: {
-    FileBrowser,
     navbar
   },
   computed: {
@@ -94,6 +124,11 @@ export default {
   },
   data() {
     return {
+      project_name: "{Your Project Name}",
+      folder_name: '',
+      object_name: '',
+      folder_list: ["Example Folder", "Example Folder 2"],
+      object_list: ["Example Object", "Example Object 2"],
       myToken: '',
       tokenPkg: {},
       treeNodePkg: {},
